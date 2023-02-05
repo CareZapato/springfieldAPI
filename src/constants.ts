a2b298d00d29f2364936326f56a5201cbf23da2c
Carezapato <pxreturn2@gmail.com>
2023-02-05 17:43:06 -0300

    stash 2

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export class Constants{
+    public URLBASE_MONGO = 'localhost';
+    public PORT_MONGO = '27017';
+    public DBNAME_MONGO = 'simpsons';
+    public COLLECTIONS_SIMPSONS_MONGO = 'characters';
+}
